b31fbbf1e092b402e2a8bbcc90322e2731d6803c
Eugenia Bonocore <eugeniabonocore@gmail.com>
2026-02-12 16:32:41 +0000

    No unused imports

--- app/src/main/java/com/example/moody/data/MoodEntry.kt
+++ app/src/main/java/com/example/moody/data/MoodEntry.kt
@@ -2,7 +2,6 @@ package com.example.moody.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.sql.Timestamp
 
 @Entity(tableName = "mood_entries")
 data class MoodEntry(
